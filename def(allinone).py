cefb9f60e12159a656bd94dfebceba98e9ad57e5
Thanu13112007 <Thanugowda1321@gmail.com>
2025-11-12 15:11:09 +0530

    def(allinone).py

---
+++ def(allinone).py
@@ -0,0 +1,9 @@
+def user_details(a,b):
+ name=input("enter your name ")
+ age=int(input("enter your age "))
+ roll_number=int(input("enter your roll number "))
+ return name,age,roll_number,age+roll_number,a*b
+user_enter your name thanvi
+enter your age 18
+enter your roll number 44
+('thanvi', 18, 44, 62, 364)details(13,28)
