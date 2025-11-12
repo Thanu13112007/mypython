65dcbe7d85cb75a6a710c8d1bcf271d1680d757e
Thanu13112007 <Thanugowda1321@gmail.com>
2025-11-12 11:55:28 +0530

    factorial.py

---
+++ factorial.py
@@ -0,0 +1,20 @@
+number=int(input("enter a number"))
+factorial=1
+for i in range(1,number+1):
+  factorial *=i
+  prinenter a number13
+1
+2
+6
+24
+120
+720
+5040
+40320
+362880
+3628800
+39916800
+479001600
+6227020800
+t(factorial)
+  
