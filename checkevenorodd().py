d6f4dc9dc681d1879b35dbfc084777ba02350dd6
Thanu13112007 <Thanugowda1321@gmail.com>
2025-11-12 20:25:25 +0530

    checkevevorodd().py
    This script defines a function to check if a number is even or odd and executes it.

---
+++ checkevenorodd().py
@@ -0,0 +1,11 @@
+def even():
+  number = int(input("Enter a number: "))
+  if number % 2 == 0:
+    print(f"{number} is even")
+  else:
+    print(f"{number} is odd")
+
+# Call the function to execute it
+even()
+Enter a number: 3
+3 is odd
