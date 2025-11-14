dcea5b33d3bfb3ad42e18cd761e0bc64c64cdaa7
Thanu13112007 <Thanugowda1321@gmail.com>
2025-11-14 14:25:17 +0530

    list5numbers.py

---
+++ list5number.py
@@ -0,0 +1,13 @@
+a=int(input("enter a number"))
+b=int(input("enter a number"))
+c=int(input("enter a number"))
+d=int(input("enter a number"))
+e=int(input("enter a number"))
+t=[a,enter a number1
+enter a number2
+enter a number3
+enter a number4
+enter a number5
+[1, 2, 3, 4, 5]
+b,c,d,e]
+print(t)
